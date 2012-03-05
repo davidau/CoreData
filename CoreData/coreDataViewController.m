49d7472b8ae476658db292b2f9c96586c9038d73
David Au <david.au@zeroflow.com>
2012-03-05 00:02:13 -0500

    Working version

--- CoreData/coreDataViewController.m
+++ CoreData/coreDataViewController.m
@@ -7,15 +7,68 @@
 //
 
 #import "coreDataViewController.h"
+#import "coreDataAppDelegate.h"
 
 @implementation coreDataViewController
 
 @synthesize name, address, phone, status;
 
 - (void) saveData {
+    coreDataAppDelegate *appDelegate = 
+    [[UIApplication sharedApplication] delegate];
+    
+    NSManagedObjectContext *context =
+    [appDelegate managedObjectContext];
+    
+    NSManagedObject *newContact;
+    newContact = [NSEntityDescription
+                  insertNewObjectForEntityForName:@"Contacts"
+                  inManagedObjectContext:context];
+    [newContact setValue:name.text forKey:@"name"];
+    [newContact setValue:address.text forKey:@"address"];
+    [newContact setValue:phone.text forKey:@"phone"];
+    name.text = @"";
+    address.text = @"";
+    phone.text = @"";
+    
+    NSError *error;
+    [context save:&error];
+    status.text = @"Contact saved";    
 }
 
 - (void) findContact {
+    coreDataAppDelegate *appDelegate = 
+    [[UIApplication sharedApplication] delegate];
+    
+    NSManagedObjectContext *context = 
+    [appDelegate managedObjectContext];
+    
+    NSEntityDescription *entityDesc = 
+    [NSEntityDescription entityForName:@"Contacts" 
+                inManagedObjectContext:context];
+    
+    NSFetchRequest *request = [[NSFetchRequest alloc] init];
+    [request setEntity:entityDesc];
+    
+    NSPredicate *pred = 
+    [NSPredicate predicateWithFormat:@"(name = %@)", 
+     name.text];
+    [request setPredicate:pred];
+    
+    NSManagedObject *matches = nil;
+    NSError *error;
+    NSArray *objects = [context executeFetchRequest:request 
+                                              error:&error];
+    
+    if ([objects count] == 0) {
+        status.text = @"No matches";
+    } else {
+        matches = [objects objectAtIndex:0];
+        address.text = [matches valueForKey:@"address"];
+        phone.text = [matches valueForKey:@"phone"];
+        status.text = [NSString stringWithFormat:
+                       @"%d matches found", [objects count]];
+    }
 }
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -48,6 +101,10 @@
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
+    self.name = nil;
+    self.address = nil;
+    self.phone = nil;
+    self.status = nil;
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
